cb21ea8bfe0bd365815e9ec927ba50cedf5a088b
daltonjatc22 <114698602+daltonjatc22@users.noreply.github.com>
2023-03-03 12:59:28 -0700

    update on loader

--- TSA-team7/js/lazy-loading.js
+++ TSA-team7/js/lazy-loading.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
-    let links = $("nav a");
-    links.click(function(event){
-        event.preventDefault();
-        console.log("prevent nav");
-    })
+    let otherPageInfo = {};
+    let links = $(".nav-wrapper a");
+    links.each((index, element) => {
+        fetch(element.href).then((response) => response.text())
+        .then((data) => {
+            console.log(data);
+            otherPageInfo[element.href] = $(data);
+            
+        });
+    });
 
+    links.click(function(evt){
+                    //YOUR CODE HERE
+                    let newPage = otherPageInfo[evt.target.href];
+                    console.log("body");
+                    console.log(newPage);
 
-})
+                    $('head *').remove();
+                    $("other-pages").css("position", "fixed")
+                        .css("bottom", "-100%").html(newPage.find("body").html());
+                
+                    evt.preventDefault();
+
+                    return false;
+                }
+            );
+});
